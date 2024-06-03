07f01926267bc71a06397ff3cc41273c0491ea55
dejailson <dejailson.pinheiro@ifma.edu.br>
2024-06-03 17:06:01 -0300

    FEAT: implementado a calculadora usando função.

---
+++ projeto_4_calculadora.py
@@ -0,0 +1,22 @@
+def calcular(num_1, num_2,oper):
+    if oper == 1:
+        return num_1 + num_2
+    elif oper == 2:
+        return num_1 - num_2
+    elif oper == 3:
+        return num_1 * num_2
+    elif oper == 4:
+        return num_1 / num_2
+    else:
+        return 'Operação inválida'
+    
+if __name__ == '__main__':
+    num_1 = int(input('Digite o primeiro número: '))
+    num_2 = int(input('Digite o segundo número: '))
+    print('1 - Somar')
+    print('2 - Subtrair')
+    print('3 - Multiplicar')
+    print('4 - Dividir')
+    oper = int(input('Digite a operação desejada: '))
+    resultado = calcular(num_1, num_2, oper)
+    print(f'O resultado da operação é: {resultado}')
