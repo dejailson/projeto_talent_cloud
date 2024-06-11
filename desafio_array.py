8c516eb8e6034e64ac3107aa936150c0a88eea8d
dejailson <dejailson.pinheiro@ifma.edu.br>
2024-06-11 13:11:45 -0300

    FIX: alterado a lógica para imprimir.

--- desafio_array.py
+++ desafio_array.py
@@ -2,5 +2,10 @@ pratos_do_dia = ['Feijoada', 'Macarronada','Lasanha','Peixada']
 ano_nas = [1980,1987,1990,2001,2002,2003,2004,2005,2006,2007,2008]
 
 if __name__ == '__main__':
-    print(pratos_do_dia)
-    print(ano_nas)
+    print('Pratos do dia')
+    for i in range(len(pratos_do_dia)):
+        print(pratos_do_dia[i])
+       
+    print('Anos de nascimento')
+    for i in range(len(ano_nas)):
+        print(ano_nas[i])
