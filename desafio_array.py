848eb6d6c0058b87493a1fcf94a7d123af2d1110
dejailson <dejailson.pinheiro@ifma.edu.br>
2024-06-11 13:06:58 -0300

    FEAT: finalizado o desafio do array em Python.

---
+++ desafio_array.py
@@ -0,0 +1,6 @@
+pratos_do_dia = ['Feijoada', 'Macarronada','Lasanha','Peixada']
+ano_nas = [1980,1987,1990,2001,2002,2003,2004,2005,2006,2007,2008]
+
+if __name__ == '__main__':
+    print(pratos_do_dia)
+    print(ano_nas)
