062dcb0bfe5d1b1eb7105da631ecdbc7af5ca254
dejailson <dejailson.pinheiro@ifma.edu.br>
2024-06-11 13:16:47 -0300

    FEAT: concluído o desafio imprimir arrays.

---
+++ desafio_imprimr_array.py
@@ -0,0 +1,5 @@
+lista_produtos = ['máscaras faciais', 'batons', 'esmaltes', 'perfumes', 'loções', 'xampus', 'sabonetes', 'delineadores'] 
+
+if __name__ == '__main__':
+    for i in range(len(lista_produtos)):
+        print(f'Temos {lista_produtos[i]} à venda!')
