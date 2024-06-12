28a6671df6e2395befb6c6b3e866e92a25928d87
dejailson <dejailson.pinheiro@ifma.edu.br>
2024-06-12 07:27:06 -0300

    FEAT: concluído o desafio quantidade de elementos no array.

---
+++ desafio_quantidade_elem_array.py
@@ -0,0 +1,7 @@
+lista_musicos = [ 'Djavan', 'Roberto Carlos', 'Elis Regina', 'Tom Jobim', 'Milton Nascimento', 'Chico Buarque', 'Nara Leão', 'Pitty', 'Simonal', 'Moacir Santos', 'Caetano Veloso', 'Elza Soares', 'Paulinho da Viola', 'Yamandú Costa', 'Gal Costa'] 
+
+if __name__ == '__main__':
+    print(f'O array possui {len(lista_musicos)} elemento(s).')
+    print(f'O segundo elemento do array é {lista_musicos[1]}')
+    print(f'O nono elemento do array é {lista_musicos[8]}')
+    print(f'O décimo quarto elemento do array é {lista_musicos[13]}')
